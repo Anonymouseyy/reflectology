226af731db24ff1587cbdf21c7fe6fd8a0ffea2e
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2023-09-29 21:15:54 -0700

    Basics

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask_session import Session
+from tempfile import mkdtemp
+import html
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Home Page"""
+
+    return render_template("index.html")
