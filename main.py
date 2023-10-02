9ee04250ecdf047c4ef1c0120607675a3cb1307d
Anthony <55033931+anthonyj33@users.noreply.github.com>
2023-10-01 22:10:54 -0700

    Bug fixing + use UID for key

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ def index():
     q = requests.get("https://zenquotes.io/api/today").json()[0]
 
     # Fetch returns objects in alphanumerical order
-    entries = entries_db.fetch().items().reverse()
+    entries = reversed(entries_db.fetch().items)
     formatted_entries = []
 
     for entry in entries:
-        day = datetime.date.fromisoformat(entry['key'])
+        day = datetime.date.fromisoformat(entry['date'])
         suf = "th"
 
         if day.day in [1, 21, 31]:
@@ -44,9 +44,9 @@ def index():
         elif day.day in [2, 22]:
             suf = "nd"
 
-        if entry["key"] == str(datetime.date.today()):
+        if entry["date"] == str(datetime.date.today()):
             display_title = "Today, " + day.strftime("%B %d") + suf + f": {entry['title']}"
-        elif entry["key"] == str(datetime.date.today() - datetime.timedelta(days=1)):
+        elif entry["date"] == str(datetime.date.today() - datetime.timedelta(days=1)):
             display_title = "Yesterday, " + day.strftime("%B %d") + suf + f": {entry['title']}"
         else:
             display_title = day.strftime("%A, %B %d") + suf + f": {entry['title']}"
@@ -62,21 +62,18 @@ def index():
     return render_template("index.html", quote=q, entries=formatted_entries)
 
 
-@app.route("/create", methods=["POST"])
+@app.route("/create", methods=["GET"])
 def create():
     """Endpoint for creating an entry"""
-    key = str(datetime.date.today())
-
-    if entries_db.get(key) is None:
-        flash("You already have an entry for today.")
-        return redirect("/")
+    date = str(datetime.date.today())
 
     data = {
+        "date": date,
         "title": "",
         "desc": "Today I...",
         "people": [],
         "places": [],
-        "file": f"{key}.json"
+        "file": ""
     }
 
     default_entry = """
@@ -88,17 +85,18 @@ def create():
             ]
         }"""
 
-    entries_db.put(data, key)
-    entries_drive.put(f"{key}.json", default_entry, "/")
+    key = entries_db.put(data)["key"]
+    entries_drive.put(f"{key}.json", default_entry)
+    entries_db.update({"file": f"{key}.json"}, key)
 
     return redirect(f"/edit?entry={key}")
 
 
-@app.route("/edit", methods=["POST"])
+@app.route("/edit", methods=["GET", "POST"])
 def edit():
     """Endpoint for creating an entry"""
     entry = entries_db.get(request.args["entry"])
-    day = datetime.date.fromisoformat(entry['key'])
+    day = datetime.date.fromisoformat(entry['date'])
     suf = "th"
 
     if day.day in [1, 21, 31]:
@@ -106,12 +104,12 @@ def edit():
     elif day.day in [2, 22]:
         suf = "nd"
 
-    if entry["key"] == str(datetime.date.today()):
-        entry["display_title"] = "Today, " + day.strftime("%B %d") + suf + f": {entry['title']}"
-    elif entry["key"] == str(datetime.date.today() - datetime.timedelta(days=1)):
-        entry["display_title"] = "Yesterday, " + day.strftime("%B %d") + suf + f": {entry['title']}"
+    if entry["date"] == str(datetime.date.today()):
+        entry["display_title"] = "Today, " + day.strftime("%B %d") + suf
+    elif entry["date"] == str(datetime.date.today() - datetime.timedelta(days=1)):
+        entry["display_title"] = "Yesterday, " + day.strftime("%B %d") + suf
     else:
-        entry["display_title"] = day.strftime("%A, %B %d") + suf + f": {entry['title']}"
+        entry["display_title"] = day.strftime("%A, %B %d") + suf
 
     content = entries_drive.get(entry["file"])
 
